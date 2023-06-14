0c890cbe4387f7aeb1f2839d199cb36d78ac2e3b
sssssn <doitforyourself0603@gmail.com>
2023-06-14 21:48:26 +0900

    feat: 강의 내용 실습 (06/12) - Lombok - 07_lombok 프로젝트 추가

---
+++ 07_lombok/src/main/java/com/example/lombok/StudentDto.java
@@ -0,0 +1,10 @@
+package com.example.lombok;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StudentDto {
+    private Long id;
+    private String name;
+    private String email;
+}

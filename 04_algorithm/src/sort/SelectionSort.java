5fcbff10e697b7d5f42cd7cfedbbd3073da1a0b9
sssssn <doitforyourself0603@gmail.com>
2023-06-14 21:39:40 +0900

    feat: 강의 내용 실습 (06/08) - 정렬 알고리즘 - BubbleSort, SelectionSort, CountingSort 추가

---
+++ 04_algorithm/src/sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {25, 12, 18, 24, 2, 21};
+        int n = arr.length;
+
+        // 가장 작은 원소를 찾아서 앞으로 보낸다.
+        for (int i = 0; i < n - 1; i++) {
+            // 1의 값이 총 정렬된 원소의 갯수, 가장 앞의 원소를 가장 작다고 설정
+            int minIndex = i;
+            // i + 1부터 마지막 원소까지 비교한다.
+            for (int j = i + 1; j < n; j++) {
+                // 가장 작은 숫자를 찾는다.
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // 가장 앞의 원소와 가장 작은 원소를 교환한다.
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}

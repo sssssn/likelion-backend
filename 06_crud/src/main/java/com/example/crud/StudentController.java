01fb79b5010d53a672a35498e6b93ee6baf4dba2
sssssn <doitforyourself0603@gmail.com>
2023-06-14 21:46:22 +0900

    feat: 강의 내용 실습 (06/08, 06/09) - Spring CRUD - 06_crud 프로젝트 추가

---
+++ 06_crud/src/main/java/com/example/crud/StudentController.java
@@ -0,0 +1,124 @@
+package com.example.crud;
+
+import com.example.crud.model.StudentDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class StudentController {
+    // StudentService 를 Controller 에서 사용
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/create-view")
+    public String createView() {
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String create(
+            @RequestParam("name")
+            String name,
+            @RequestParam("email")
+            String email
+    ) {
+        System.out.println(name);
+        System.out.println(email);
+        StudentDto studentDto
+                = studentService.createStudent(name, email);
+        System.out.println(studentDto.toString());
+//        return "redirect:/create-view";
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute(
+                "studentList",
+                studentService.readStudentAll()
+        );
+        return "home";
+    }
+
+    @GetMapping("/{id}")
+    public String read(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+//        studentService.readStudent(id);
+        model.addAttribute(
+                "student",
+                studentService.readStudent(id)
+        );
+        return "read";
+    }
+
+    // TODO url 설정 / ("/{id}/edit") 또는 ("/{id}/update-view") / @GetMapping
+    @GetMapping("/{id}/update-view")
+    public String updateView(
+            // TODO 아이디와 Model 받아오기 / Long id, Model model
+            @PathVariable("id") Long id,
+            Model model
+    ){
+        // TODO Model에 student 데이터 부여 / studentService.readStudent
+        model.addAttribute(
+                "student",
+                studentService.readStudent(id)
+        );
+
+        // TODO update.html 돌려주기 / "update"
+        return "update";
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(
+            // TODO StudentController.readOne()를 참조
+            @PathVariable("id") Long id,
+            // TODO StudentController.create()를 참조
+            @RequestParam("name") String name,
+            @RequestParam("email") String email
+    ) {
+        // service 활용하기
+        studentService.updateStudent(id, name, email);
+        // 상세보기 페이지로 redirect
+        return String.format("redirect:/%s", id);
+    }
+
+    // TODO
+    // deleteView 메소드 만들기
+    // GetMapping 을 써서...
+    // Long id는 어떻게...
+    // studentDto 를 가지고...
+    // return...
+    @GetMapping("/{id}/delete-view")
+    public String deleteView(
+            @PathVariable("id")
+            Long id,
+            Model model
+    ) {
+        StudentDto studentDto
+                = studentService.readStudent(id);
+        model.addAttribute("student", studentDto);
+        return "delete";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(
+            @PathVariable("id")
+            Long id
+    ) {
+        studentService.deleteStudent(id);
+        // update 때는 데이터가 남아있지만
+        // delete 는 돌아갈 상세보기가 없다
+        // 그래서 홈으로 돌아간다.
+        return "redirect:/home";
+    }
+}
---
+++ 06_crud/src/main/java/com/example/crud/StudentService.java
@@ -0,0 +1,76 @@
+package com.example.crud;
+
+import com.example.crud.model.StudentDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    // 복수의 StudentDto 를 담는 멤버변수 (필드
+    private final List<StudentDto> studentList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public StudentService(){
+        createStudent("alex", "alex@gmail.com");
+        createStudent("brad", "brad@gmail.com");
+        createStudent("chad", "chad@gmail.com");
+    }
+
+    // 새로운 StudentDto 를 생성하는 메서드
+    public StudentDto createStudent(String name, String email) {
+        StudentDto newStudent = new StudentDto(
+                nextId, name, email
+        );
+        nextId++;
+        studentList.add(newStudent);
+        return newStudent;
+    }
+
+    public List<StudentDto> readStudentAll() {
+        return studentList;
+    }
+
+    // 단일 StudentDto를 주는 메소드
+    public StudentDto readStudent(Long id) {
+        for (StudentDto studentDto: studentList) {
+            if (studentDto.getId().equals(id))
+                return studentDto;
+        }
+
+        return null;
+    }
+
+    // 어떤 학생 데이터를 갱신할 것인지
+    // 그 학생의 갱신될 데이터
+    public StudentDto updateStudent(Long id, String name, String email){
+        StudentDto targetDto = this.readStudent(id);
+        if (targetDto != null) {
+            targetDto.setName(name);
+            targetDto.setEmail(email);
+            return targetDto;
+        } else return null;
+    }
+
+    public boolean deleteStudent(Long id) {
+        int target = -1;
+        // 학생 리스트를 살펴보며
+        for (int i = 0; i < studentList.size(); i++) {
+            // 대상을 선정한다.
+            if (studentList.get(i).getId().equals(id)) {
+                target = i;
+                break;
+            }
+        }
+
+        // 검색 성공시
+        if (target != -1){
+            // 삭제
+            studentList.remove(target);
+            return true;
+        }
+        else return false;
+    }
+}

3b42b585928b059dc973e648f5f20a0ab97e5ff0
sssssn <doitforyourself0603@gmail.com>
2023-06-22 16:10:06 +0900

    fix: 강의 내용 실습 - Comment 구현 기능 추가

--- 12_article-skeleton/src/main/java/com/example/article/CommentController.java
+++ 12_article-skeleton/src/main/java/com/example/article/CommentController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/articles/{articleId}/comments")
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService service;
 
+    // 게시글 작성
     // POST /articles/{articleId}/comments
     @PostMapping
     public CommentDto create(
@@ -19,5 +22,37 @@ public class CommentController {
             @RequestBody CommentDto dto
     ) {
         return service.createComment(articleId, dto);
+    }
+
+    // 게시글 댓글 전체 조회
+    // GET /articles/{articleId}/comments
+    // 반환값 인자 및 필요 어노테이션
+    @GetMapping
+    public List<CommentDto> readAll(
+            @PathVariable("articleId") Long articleId
+    ) {
+        return service.readCommentAll(articleId);
+    }
+
+    // 게시글 댓글 수정
+    // PUT /articles/{articleId}/comments/{commentId}
+    // RequestMapping 부터 return 까지
+    @PutMapping("/{commentId}")
+    public CommentDto update(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("commentId") Long commentId,
+            @RequestBody CommentDto dto
+    ) {
+        return service.updateComment(articleId, commentId, dto);
+    }
+
+    // 게시글 댓글 삭제
+    // DELETE /articles/{articleId}/comments/{commentId}
+    @DeleteMapping("/{commentId}")
+    public void delete(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("commentId") Long commentId
+    ) {
+        service.deleteComment(articleId, commentId);
     }
 }

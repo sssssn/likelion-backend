14d263a79501f45c6e0062846539b29c419ff988
sssssn <doitforyourself0603@gmail.com>
2023-06-27 16:23:40 +0900

    feat: 강의 내용 실습 - AOP(Aspect Oriented Programming) - 16_aop 프로젝트 추가

---
+++ 16_aop/src/main/java/com/example/aop/aspect/LogExecutionTime.java
@@ -0,0 +1,12 @@
+package com.example.aop.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogExecutionTime {
+    // 내용은 없어도 됨
+}
